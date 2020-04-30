768a3e8142f9faeb16ba8cd1aa592d3251fb9286
Nai Bing Jiang <naibingjiang@gmail.com>
2020-04-30 17:19:46 -0400

    updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartupComponent } from './components/startup/startup.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartupComponent
+    StartupComponent,
+    SignupComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
