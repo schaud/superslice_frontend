32dd80a108b6bb1c9b737d7bb4c041f78157280f
Jesse Rivera <35208196+codiene@users.noreply.github.com>
2020-04-30 16:08:58 -0400

    added bootstrap and jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartupComponent } from './components/startup/startup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartupComponent
   ],
   imports: [
     BrowserModule,
