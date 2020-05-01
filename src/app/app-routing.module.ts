fc23e140db5a7e22f410449dbf6190bb17a16db8
Nai Bing Jiang <naibingjiang@gmail.com>
2020-05-01 15:27:56 -0400

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { UserfinderComponent } from './components/userfinder/userfinder.component';
 
 
 const routes: Routes = [
   {path:'about-us', component: AboutUsComponent},
   {path:'contact-us', component: ContactUsComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: SignupComponent}
+  {path: 'register', component: SignupComponent},
+  {path: 'user', component: UserfinderComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserfinderComponent } from './components/userfinder/userfinder.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { HeaderComponent } from './components/header/header.component';
     FooterComponent,
     AboutUsComponent,
     ContactUsComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserfinderComponent
   ],
   imports: [
     BrowserModule,
