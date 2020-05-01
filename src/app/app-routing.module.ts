a8fcacc44d4fd5f53136b7bce512150dda44e316
Nai Bing Jiang <naibingjiang@gmail.com>
2020-05-01 11:31:18 -0400

    added route for about us and contact us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about-us', component: AboutUsComponent},
+  {path:'contact-us', component: ContactUsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
