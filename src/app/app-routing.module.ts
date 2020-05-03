adb5f97f9ab7954e10cbdd8757d3599869c91b84
Nai Bing Jiang <naibingjiang@gmail.com>
2020-05-02 20:12:26 -0400

    fix home routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path:'about-us', component: AboutUsComponent},
   {path:'contact-us', component: ContactUsComponent},
   {path: 'login', component: LoginComponent},
